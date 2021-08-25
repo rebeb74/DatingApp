4d39fd97a01c278cf50e618edd0ed11043c8d078
rebeb74 <66473575+rebeb74@users.noreply.github.com>
2021-08-25 15:25:10 +0200

    end of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -6,8 +6,29 @@ namespace API.Data
     public class DataContext : DbContext
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
         public DataContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(key => new {key.SourceUserId, key.LikedUserId});
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
